d45ffa68383402d9fb1012afabce2ed06fc6f985
Alex Holmes <alex_j_holmes@outlook.com>
2024-05-26 14:04:04 +0100

    Added various new styling and map adjustments

--- libs/js/leaflet.js
+++ libs/js/leaflet.js
@@ -1,15 +1,128 @@
 
 
 
-// var map = L.map('map').setView([latt, long], 13); // This needs input, the below finds your location.
+// var map = L.map('map').setView([51.509865, -0.118092], 13); // This needs input, the below finds your location.
 
-var map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 13});
+var map = L.map('map', {
+    doubleClickZoom: false,
+}).locate({
+    setView: true, 
+    maxZoom: 13,
+});
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+
+// Everything below here is test data - Everything above is running the map
+
+var infoBtn = L.easyButton("fa-info", function (btn, map) {
+    $("#mapModal").modal("show");
+  });
+
+infoBtn.addTo(map);
+
+var infoBtnAirport = L.easyButton("fa-plane", function (btn, map) {
+    $("#countryAirports").modal("show");
+  });
+
+infoBtnAirport.addTo(map);
+
+var infoBtnFootball = L.easyButton("fa-futbol-o", function (btn, map) {
+    $("#countryFootball").modal("show");
+  });
+
+infoBtnFootball.addTo(map);
+
+
+// Tiles
+
+var leedsUnited = L.marker([53.777782, -1.573049]).bindPopup('Leeds United FC');
+var liversedgeFc = L.marker([53.717252131, -1.70706217173]).bindPopup('Liversedge FC');
+
+// Tile Groups
+
+var stadiums = L.layerGroup([leedsUnited, liversedgeFc]);
+
+
+// Map Layers
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 maxZoom: 19,
 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var OpenStreetMap_France = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
+}).addTo(map);
+
+
+var baseLayers = {
+    "Satalite": Stadia_AlidadeSatellite,
+    "Open Map France": OpenStreetMap_France,
+    "OpenStreetMap": osm, 
+    
+};
+
+var overlays = {
+    "Grounds": stadiums
+};
+
+L.control.layers(baseLayers, overlays).addTo(map);
 
 
 
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+var someFeatures = [{
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "show_on_map": true
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Busch Field",
+        "show_on_map": true
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-103.98404, 38.74621]
+    }
+}];
+
+
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+L.geoJSON(someFeatures, {
+    filter: function(feature, layer) {
+        return feature.properties.show_on_map;
+    }
+}).addTo(map);
--- libs/js/script.js
+++ libs/js/script.js
@@ -86,7 +86,180 @@ $(document).ready(function() {
                     });
 
     }
+        // This call defaults the initial modal buttons to GB
+        $.ajax({
+            url: "./libs/php/countryInformation.php",
+            type: 'POST',
+            dataType: 'json',
+            data: { 
+                countryCode: $('#countrySelect').val()
+    
+            },
+            success: function(result) {
+    
+                console.log(JSON.stringify(result));
+    
+                if (result.status.name == 'ok') {
+                        
+                    $('#flag-icon').html(result['data'][0]['flag']);
+    
+                    
+                    // Icon images
+                    var flag = (result['data'][0]['flags']['svg']);                
+                    $('#main-country-flag').attr('src', flag);
+            
+                    var coa = (result['data'][0]['coatOfArms']['svg']);                
+                    $('#coat-of-arms').attr('src', coa);
+                    
+                    
+                }
+    
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                
+                console.log(jqXHR);
+            }
+    
+    })
 
-    // New API call under here
+    $.ajax({
+        url: "./libs/php/countryStatistics.php",
+        type: 'POST',
+        dataType: 'json',
+        data: { 
+            countryCode: $('#countrySelect').val()
+
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == 'ok') {
+                    
+                $('#countryPopulation').html(result['data'][0]['population']);
+                $('#countryUnemployment').html(result['data'][0]['unemployment']);
+                $('#countryHomicideRate').html(result['data'][0]['homicide_rate']);
+                $('#countryCurrency').html(result['data'][0]['currency']['name']);
+                $('#countryMaleLifeExpectancy').html(result['data'][0]['life_expectancy_male']);
+                $('#countryFemaleLifeExpectancy').html(result['data'][0]['life_expectancy_female']);
+                $('#countryCapital').html(result['data'][0]['capital']);
+                $('#countryCo2Emissions').html(result['data'][0]['co2_emissions']);
+                $('#countrySurfaceArea').html(result['data'][0]['surface_area']);
+                
+                
+            }
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            
+            console.log(jqXHR);
+        }
+
+    })
+        
 
 });
+
+$('#btnCountries').click(function() {
+
+    if (document.getElementById('countrySelect').value === 'gbr') {
+        map.flyTo([51.509865, -0.118092], 8)
+    } else if (document.getElementById('countrySelect').value === 'usa') {
+        map.flyTo([38.907192, -77.036873], 8)
+    } else if (document.getElementById('countrySelect').value === 'aus') {
+        map.flyTo([-35.282001, 149.128998], 8)
+    } else if (document.getElementById('countrySelect').value === 'chn') {
+        map.flyTo([39.916668, 116.383331], 8)
+    } else if (document.getElementById('countrySelect').value === 'rus') {
+        map.flyTo([55.751244, 37.618423], 8)
+    } else if (document.getElementById('countrySelect').value === 'ind') {
+        map.flyTo([28.644800, 77.216721], 8)
+    };
+
+
+    $.ajax({
+        url: "./libs/php/countryInformation.php",
+        type: 'POST',
+        dataType: 'json',
+        data: { 
+            countryCode: $('#countrySelect').val()
+
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == 'ok') {
+                    
+                $('#flag-icon').html(result['data'][0]['flag']);
+
+                
+                // Icon images
+                var flag = (result['data'][0]['flags']['svg']);                
+                $('#main-country-flag').attr('src', flag);
+        
+                var coa = (result['data'][0]['coatOfArms']['svg']);                
+                $('#coat-of-arms').attr('src', coa);
+                
+                
+            }
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            
+            console.log(jqXHR);
+        }
+
+})
+    
+
+    $.ajax({
+        url: "./libs/php/countryStatistics.php",
+        type: 'POST',
+        dataType: 'json',
+        data: { 
+            countryCode: $('#countrySelect').val()
+
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == 'ok') {
+                    
+                $('#countryPopulation').html(result['data'][0]['population']);
+                $('#countryUnemployment').html(result['data'][0]['unemployment']);
+                $('#countryHomicideRate').html(result['data'][0]['homicide_rate']);
+                $('#countryCurrency').html(result['data'][0]['currency']['name']);
+                $('#countryMaleLifeExpectancy').html(result['data'][0]['life_expectancy_male']);
+                $('#countryFemaleLifeExpectancy').html(result['data'][0]['life_expectancy_female']);
+                $('#countryCapital').html(result['data'][0]['capital']);
+                $('#countryCo2Emissions').html(result['data'][0]['co2_emissions']);
+                $('#countrySurfaceArea').html(result['data'][0]['surface_area']);
+                
+                
+            }
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            
+            console.log(jqXHR);
+        }
+
+    })
+
+    
+
+
+});
+
+
+// Location Buttons
+
+$('#countryUsa').click(function() {
+    map.flyTo([40.737, -73.923], 8)
+})
+
+$('#countryUk').click(function() {
+    map.flyto([51.509865, -0.118092], 8)
+})
